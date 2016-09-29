32195dbde93f22779ffa1a13acdb7a9efbe8ebf8
andrewjpage <andrewjpage@gmail.com>
2016-09-29 11:53:14 +0100

    update version

--- gff3toembl.rb
+++ gff3toembl.rb
@@ -1,10 +1,11 @@
 class Gff3toembl < Formula
   desc "Convert GFF3 files to a format suitable for submission to EMBL"
   homepage "https://github.com/sanger-pathogens/gff3toembl"
-  url "https://github.com/sanger-pathogens/gff3toembl/archive/v1.0.0.tar.gz"
-  sha256 "a224e5406098714a12d257e307239120150e80b8b75fcf2f77149ed034757f38"
-  head "https://github.com/sanger-pathogens/gff3toembl.git"
   # tag "bioinformatics"
+
+  url "https://github.com/sanger-pathogens/gff3toembl/archive/v1.1.0.tar.gz"
+  sha256 "f5ec9e08660519be6be22b487c9acceabb629bb19c8e45bc09df74d15b2bdae7"
+  head "https://github.com/sanger-pathogens/gff3toembl.git"
 
   depends_on :python
   depends_on "genometools"
